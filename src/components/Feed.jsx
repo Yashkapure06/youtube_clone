22c7fc8f63f3acd48a6c6a3e0578fe53e88cafc9
Yashkapure06 <61585443+Yashkapure06@users.noreply.github.com>
2022-08-28 17:56:54 +0530

    useEffect Hook used for fetching data and storing

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,19 @@
 import React, { useState, useEffect } from "react";
 import { Box, Stack, Typography } from "@mui/material";
-import Sidebar from "./Sidebar";
+import { Sidebar, Videos } from "./";
+
+import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const Feed = () => {
+  const [selectedCategory, setselectedCategory] = useState("New");
+  const [videos, setVideos] = useState([]);
+
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+      .then((data) => setVideos(data.items))
+  }, [selectedCategory]);
+
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
       <Box
@@ -12,17 +23,32 @@ const Feed = () => {
           px: { sx: 0, md: 2 },
         }}
       >
-        <Sidebar />
-        <Typography 
-          className="copyright" 
+        <Sidebar 
+          selectedCategory={selectedCategory}
+          setselectedCategory={setselectedCategory}
+        />
+        <Typography
+          className="copyright"
           variant="body2"
           sx={{
-            mt:1.5,
-            color:'#fff'
-          }}    
+            mt: 1.5,
+            color: "#fff",
+          }}
         >
           Copyright &copy; 2022 Youtube
         </Typography>
+      </Box>
+      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{ color: "#fff" }}
+        >
+        {selectedCategory}
+          <span style={{ color: "#f31503" }}> Videos</span>
+        </Typography>
+        <Videos videos={videos} />
       </Box>
     </Stack>
   );
