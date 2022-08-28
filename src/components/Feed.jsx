8293635eed57e364dee2177144d82cb00f0230c8
Yashkapure06 <61585443+Yashkapure06@users.noreply.github.com>
2022-08-28 12:21:59 +0530

    imported Sidebar component

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Box, Stack, Typography } from "@mui/material";
+import Sidebar from "./Sidebar";
 
 const Feed = () => {
   return (
@@ -11,7 +12,7 @@ const Feed = () => {
           px: { sx: 0, md: 2 },
         }}
       >
-        Sidebar
+        <Sidebar />
         <Typography 
           className="copyright" 
           variant="body2"
