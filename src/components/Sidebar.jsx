6cd4da3404126cb0ed8b8fc6575dd5acfdd7d838
Yashkapure06 <61585443+Yashkapure06@users.noreply.github.com>
2022-08-28 17:58:02 +0530

    Made dynamic -> select category name  for each field

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,9 +3,8 @@ import { Stack } from "@mui/material";
 
 import { categories } from "../utils/constants";
 
-const selectedCategory = "New";
 
-const Sidebar = () => (
+const Sidebar = ({ selectedCategory, setselectedCategory }) => (
   <Stack
     direction="row"
     sx={{
@@ -17,6 +16,7 @@ const Sidebar = () => (
     {categories.map((category) => (
       <button
         className="category-btn"
+        onClick={()=>setselectedCategory(category.name)}
         style={{
           background: category.name === selectedCategory && "#FC1503",
           color: "white",
