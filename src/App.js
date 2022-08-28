92fb3bdf3659a0dff9c96cd16001dfbaca153254
Yashkapure06 <61585443+Yashkapure06@users.noreply.github.com>
2022-08-29 00:16:18 +0530

    Bug Check

--- src/App.js
+++ src/App.js
@@ -16,8 +16,9 @@ const App = () => (
       <Routes>
         <Route path="/" exact element={<Feed />} />
         <Route path="/video/:id" element={<VideoDetail />} />
-        <Route path="/video/:id" element={<ChannelDetail />} />
-        <Route path="/search/:searchTerm" element={<SearchFeed />} />
+        <Route path="/channel/:id" element={<ChannelDetail />} />
+        
+        <Route path='/search/:searchTerm' element={<SearchFeed />} />
       </Routes>
     </Box>
   </BrowserRouter>
