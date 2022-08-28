51e29fb9bf1b80c02bd6279e05f0dee9d910715e
Yashkapure06 <61585443+Yashkapure06@users.noreply.github.com>
2022-08-28 17:56:12 +0530

    imported Channel Card And video Card in index

--- src/components/index.js
+++ src/components/index.js
@@ -5,3 +5,6 @@ export { default as SearchFeed } from "./SearchFeed";
 export { default as VideoDetail } from "./VideoDetail";
 export { default as SearchBar } from "./SearchBar";
 export { default as Sidebar } from "./Sidebar";
+export { default as Videos } from "./Videos";
+export { default as VideoCard } from "./VideoCard";
+export { default as ChannelCard } from "./ChannelCard";
