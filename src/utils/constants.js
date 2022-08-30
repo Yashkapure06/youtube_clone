69c4c98158e5341440ce42ab3e3104d4d0c671a9
Yashkapure06 <61585443+Yashkapure06@users.noreply.github.com>
2022-08-30 10:23:51 +0530

    bug check

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -16,7 +16,6 @@ export const logo = 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/Y
 
 export const categories = [
   { name: 'New', icon: <HomeIcon />, },
-  { name: 'JS Mastery', icon: <CodeIcon />, },
   { name: 'Coding', icon: <CodeIcon />, },
   { name: 'ReactJS', icon: <CodeIcon />, },
   { name: 'NextJS', icon: <CodeIcon />, },
